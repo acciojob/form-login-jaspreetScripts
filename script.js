405cff2fce43c3c303d5b4a5571f1b238c921059
jaspreetScripts <119395932+jaspreetScripts@users.noreply.github.com>
2023-06-22 11:27:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function getFormvalue() {
     //Write your code here
 	const form= document.getElementById("form1");
-	 alert (form[0]+" "+form[1]);
+	 const fName= form.elemnets["fname"].value;
+	const lName= form.elements["lName"].value;
+	alert(fName +" "+lName);
 
 }
