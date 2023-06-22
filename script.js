502812cf9c0bfadca9beb935b9d481aaff0fc7da
jaspreetScripts <119395932+jaspreetScripts@users.noreply.github.com>
2023-06-22 11:29:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getFormvalue() {
     //Write your code here
 	const form= document.getElementById("form1");
 	 const fName= form.elemnets["fname"].value;
-	const lName= form.elements["lName"].value;
+	const lName= form.elements["lname"].value;
 	alert(fName +" "+lName);
 
 }
